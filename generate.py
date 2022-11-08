d48f8fdd5107344490b90f23dc9141ae1f6f0591
yum <yum.food.vr@gmail.com>
2022-11-07 17:50:30 -0800

    Add generate.py
    Generates a string with every character starting from a minimum. Useful
    for testing paging and font issues.

---
+++ generate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+import osc_ctrl
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", default="127.0.0.1", help="OSC server IP")
+parser.add_argument("-p", type=int, default=9000, help="OSC server port")
+args = parser.parse_args()
+
+state = osc_ctrl.EvilGlobalState()
+client = osc_ctrl.getClient(args.i, args.p)
+tx_state = osc_ctrl.OscTxState()
+state.encoding = osc_ctrl.generateEncoding()
+
+osc_ctrl.clear(client)
+
+i = 0xC000
+line = ""
+while True:
+    for j in range(0, 256):
+        letter = chr(i)
+        line += letter
+        i = i + 1
+
+    while not osc_ctrl.sendMessageLazy(client, line, tx_state):
+        continue
+
