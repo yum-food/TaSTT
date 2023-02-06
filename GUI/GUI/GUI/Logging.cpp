64239b6949ad185c62f4a86acaced9f811d54e9e
yum <yum.food.vr@gmail.com>
2023-02-06 15:38:20 -0800

    Improve PII filter
    Based on screenshots seen in Discord. This filter is just here to
    maximize user privacy while debugging.

--- GUI/GUI/GUI/Logging.cpp
+++ GUI/GUI/GUI/Logging.cpp
@@ -6,7 +6,7 @@
 std::string Logging::HidePII(const std::string&& str,
 	const std::string& replacement) {
 	try {
-		std::regex c_users("(C:\\\\+Users\\\\+)[a-zA-Z0-9_ ]+");
+		std::regex c_users("([A-Za-z]:\\\\+[Uu]sers\\\\+)[a-zA-Z0-9_ ]+");
 		std::string real_replacement = "$1" + replacement;
 		return std::regex_replace(str, c_users, real_replacement);
 	}
